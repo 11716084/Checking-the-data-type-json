f8c9513b85feffd8f3d385325546a7cef7f9f307
Krysia <kertyucwerty@gmail.com>
2021-02-17 18:41:49 +0100

    mess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import re
 # x = input()
 # with open("data_file.json", "w") as write_file:
 #   json.dump(write_file)
@@ -7,31 +8,22 @@ with open("data_file.json", "r") as json_file:
   data = json.load(json_file)
 # data = json.loads(x)
 len = len(data)
-bus_error = 0
-stop_id_error = 0
 stop_name_error = 0
-next_stop_error = 0
 stop_type_error = 0
 a_time_error = 0
 for i in range(0, len):
-  if type(data[i]["bus_id"]) is not int:
-    bus_error += 1
-  if type(data[i]["stop_id"]) is not int:
-    stop_id_error += 1
-  if type(data[i]["stop_name"]) is not str or data[i]["stop_name"] == "":
-    stop_name_error += 1  # here is not vsalid
-  if type(data[i]["next_stop"]) is not int:
-    next_stop_error += 1
-  if type(data[i]["stop_type"]) is not str or data[i]["stop_type"] != 'F' and data[i]["stop_type"] != 'S' and data[i]["stop_type"] != 'O' and data[i]["stop_type"] != '':
-    stop_type_error += 1
-  if type(data[i]["a_time"]) is not str or data[i]["a_time"] == "":
-    a_time_error += 1
-sum = bus_error + stop_id_error + stop_name_error + next_stop_error + stop_type_error + a_time_error
+  template_stop_name = r"^[A-Z].+ (Street|Avenue|Boulevard|Road)+$"
+  template_stop_type = r'^[S,O,F]$|^$'
+  template_a_time = r'(^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$)'
+  if re.match(template_stop_name, (data[i]["stop_name"])) is None:
+    stop_name_error += 1    #The stop name must end on Street, Avenue, Boulevard or Road
+  if re.match(template_stop_type, (data[i]["stop_type"])) is None:
+    stop_type_error += 1    #Stop_type could be: S, O, F or an empty string
+  if re.match(template_a_time, (data[i]["a_time"])) is None:
+    a_time_error += 1  #  Military hour goes from 00:00 to 23:59, I suggest use (element1|element2|element3) for the first cluster of hour.
+sum = stop_name_error + stop_type_error + a_time_error
 
-print(f"""Type and required field validation: {sum} errors
-bus_id: {bus_error}
-stop_id: {stop_id_error}
+print(f"""Format validation: {sum} errors
 stop_name: {stop_name_error}
-next_stop: {next_stop_error}
 stop_type: {stop_type_error}
-a_time: {a_time_error}""")
+a_time: {a_time_error} """)
